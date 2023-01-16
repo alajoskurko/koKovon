7824c3813d813120edd1ead2694d1ff7d4ab7777
Kurko Alajos <alajos@ether.cards>
2023-01-16 12:15:40 +0200

    scale in scanc camera image

--- Assets/Scirpts/Feature2DExample.cs
+++ Assets/Scirpts/Feature2DExample.cs
@@ -44,6 +44,9 @@ namespace OpenCVForUnityExample
         [SerializeField]
         RawImage referencePanel;
 
+        [SerializeField]
+        Canvas mainCanvas;
+
         int counter = 0;
 
         public float deltaTime;
@@ -58,12 +61,24 @@ namespace OpenCVForUnityExample
             /// set the workers for the separated threads
             SetBackgroundWorkers();
             panelBg.GetComponent<RawImage>().texture = backCam;
-            panelBg.rectTransform.sizeDelta = new Vector2(referencePanel.rectTransform.rect.height, referencePanel.rectTransform.rect.width);
-            Debug.Log(referencePanel.rectTransform.rect + "rect x");
+            // panelBg.rectTransform.sizeDelta = new Vector2(referencePanel.rectTransform.rect.height, referencePanel.rectTransform.rect.width);
+            panelBg.rectTransform.sizeDelta = new Vector2(backCam.width, backCam.height);
+            Debug.LogWarning(panelBg.rectTransform.sizeDelta.x / panelBg.rectTransform.sizeDelta.y  + "elotte");
+            //Debug.LogWarning(referencePanel.GetComponent<RawImage>().rectTransform.rect.height + " height");
+            //Debug.LogWarning(panelBg.rectTransform.GetComponent<RawImage>().rectTransform.rect.height + " height");
+            //Debug.LogWarning(mainCanvas.GetComponent<RectTransform>().rect.height + " height");
+            //Debug.LogWarning(panelBg.rectTransform.rect.width + " height");
 
-            #if UNITY_IPHONE
+            float heightDifference = 100 - (panelBg.rectTransform.rect.width * 100 / mainCanvas.GetComponent<RectTransform>().rect.height);
+            float diffInScale = 1 + (heightDifference / 100);
+            panelBg.rectTransform.localScale = new Vector3(diffInScale, diffInScale, diffInScale);
+            Debug.LogWarning(panelBg.rectTransform.sizeDelta.x / panelBg.rectTransform.sizeDelta.y + "utana");
+            //Debug.LogWarning(diffInScale + " diffInScale");
+
+
+#if UNITY_IPHONE
             panelBg.transform.localScale = new Vector3(1, -1, 1);
-            #endif
+#endif
             progressController = MainController.Instance.progressController;
             SwipeDetector.OnSwipe += SwipeDetector_OnSwipe;
             
@@ -107,7 +122,7 @@ namespace OpenCVForUnityExample
 
                     //decrease webcamtexture size for a better fps performance
                     //if there is something wrong with the symbol scan, put back the 500, 885
-                    backCam = new WebCamTexture(devices[0].name,400, 885);
+                    backCam = new WebCamTexture(devices[0].name,1000, 485);
                     //System.GC.Collect();
                     //Debug.Log(GarbageCollector.GCMode + " gb gcmode");
 
